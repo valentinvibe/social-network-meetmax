5f77e6c910584f61eee1c003436f27a3f3c7d6a3
Valentin Kulakov <7705678@gmail.com>
2022-10-31 09:36:21 +0300

    feat: profile intro

---
+++ src/components/profile-page/components/intro/intro.jsx
@@ -0,0 +1,56 @@
+import styles from './intro.module.css';
+
+import planet from '../../../../images/icons/profile-intro/planet.svg';
+import profile from '../../../../images/menu-icons/profile.svg';
+import birthday from '../../../../images/icons/profile-intro/birthday.svg';
+import location from '../../../../images/icons/profile-intro/location.svg';
+import vk from '../../../../images/icons/profile-intro/vk.svg';
+import fb from '../../../../images/icons/profile-intro/fb.svg';
+import instagram from '../../../../images/icons/profile-intro/instagram.svg';
+
+const Intro = () => {
+    return(
+        <div className={styles.wrapper}>
+            <h2 className={styles.title}>intro</h2>
+            <ul className={styles.list}>
+                <li className={styles.listItem}>
+                    <img className={styles.ico} src={planet} alt="website"/>
+                    uihut.com
+                </li>
+                <li className={styles.listItem}>
+                    <img className={styles.ico} src={profile} alt="profile"/>
+                    Male
+                </li>
+                <li className={styles.listItem}>
+                    <img className={styles.ico} src={birthday} alt="birthday"/>
+                    Born May 5, 1989
+                </li>
+                <li className={styles.listItem}>
+                    <img className={styles.ico} src={location} alt="location"/>
+                    Russia, Moscow
+                </li>
+                <li className={styles.listItem}>
+                    <img className={styles.ico} src={vk} alt="vk"/>
+                    <a href="/">valentin</a>
+                </li>
+                <li className={styles.listItem}>
+                    <img className={styles.ico} src={fb} alt="fb"/>
+                    <a href="/">valentin</a>
+                </li>
+                <li className={styles.listItem}>
+                    <img className={styles.ico} src={instagram} alt="instagram"/>
+                    <a href="/">@valentin</a>
+                </li>
+                <li className={styles.listItem}>
+                    58,975 Followers
+                </li>
+                <li className={styles.listItem}>
+                    2,564 Following
+                </li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Intro
--- src/components/profile-page/profile-page.jsx
+++ src/components/profile-page/profile-page.jsx
@@ -1,31 +1,52 @@
-import styles from './profile-page.module.css';
-import ava from '../../images/avatar.jpg';
+import styles from "./profile-page.module.css";
+import ava from "../../images/avatar.jpg";
+import eye from "../../images/icons/eye-visible.svg";
+import upload from "../../images/icons/upload.svg";
+import Intro from "./components/intro/intro";
 
 const Profile = () => {
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.header}>
-                <div className={styles.headerImage}>
-              
-                </div>
-                
-                <div className={styles.about}>
-                    <div className={styles.avatar}>
-                        <img className={styles.profileAvatar} src={ava} alt='avatar'/>
-                        <p className={styles.paragraph}>Name Surname</p>
-                        <p className={styles.paragraph}>Frontend Developer</p>
-                    </div>
-                    <div className={styles.edit}>
-                        <img className={styles.ico} src="" alt="" />
-                        <button type='button' className={styles.editBtn}>Edit basic info</button>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.content}>
-
-            </div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <div className={styles.headerImage}>
+          <img className={styles.profileAvatar} src={ava} alt="avatar" />
+          <button type="button" className={styles.changeBackground}>
+            <img className={styles.uploadIco} src={upload} alt="upload" />
+            Edit Cover Photo
+          </button>
         </div>
-    );
-}
+
+        <div className={styles.about}>
+          <div className={styles.avatar}>
+            <p className={`${styles.paragraph} ${styles.userName}`}>
+              Valentin Kulakov
+            </p>
+            <p className={`${styles.paragraph} ${styles.userAbout}`}>
+              Frontend Developer
+            </p>
+          </div>
+          <div className={styles.edit}>
+            <img className={styles.ico} src={eye} alt="eye" />
+            <button type="button" className={styles.editBtn}>
+              Edit basic info
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.sidebar}>
+            <Intro/>
+        </div>
+        <div className={styles.mainContent}>
+
+        </div>
+        <div className={styles.rightSideBar}>
+
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
