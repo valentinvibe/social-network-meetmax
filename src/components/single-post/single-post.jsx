ffca79cbbe880e6f20fa74abdadbaa367ff00b41
Valentin Kulakov <7705678@gmail.com>
2022-11-04 01:37:24 +0300

    feat: block comment

--- src/components/single-post/single-post.jsx
+++ src/components/single-post/single-post.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./single-post.module.css";
 import Button from "react-bootstrap/Button";
 import avatar from "../../images/avatar.jpg";
+import notlike from "../../images/icons/notlike.svg";
+import comment from "../../images/icons/comment.svg";
+import share from "../../images/icons/share.svg";
+import likeBlue from "../../images/icons/like-blue.svg";
+import send from "../../images/icons/send.svg";
+
+import Form from 'react-bootstrap/Form';
+
 
 const SinglePost = () => {
+  const [isLike, setLike] = useState(true);
+
+  const handleLike = () => {
+     return isLike ? setLike(false) : setLike(true)
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
@@ -31,20 +45,33 @@ const SinglePost = () => {
       </div>
 
       <div className={styles.reaction}>
+        {isLike ? (
         <Button
           size="sm"
           variant="outline-primary"
           className={styles.reactionBtn}
+          onClick={handleLike}
         >
-          {/* <img src={} alt="live video" className={styles.ico} /> */}
+          <img src={likeBlue} alt="live video" className={styles.ico} />
           Like
         </Button>
+        ) : (
+          <Button
+          size="sm"
+          variant="outline-secondary"
+          className={styles.reactionBtn}
+          onClick={handleLike}
+        >
+          <img src={notlike} alt="live video" className={styles.ico} />
+          Like
+          </Button>
+          )}
         <Button
           size="sm"
           variant="outline-secondary"
           className={styles.reactionBtn}
         >
-          {/* <img src={} alt="live video" className={styles.ico} /> */}
+          <img src={comment} alt="live video" className={styles.ico} />
           Comments
         </Button>
         <Button
@@ -52,10 +79,18 @@ const SinglePost = () => {
           variant="outline-secondary"
           className={styles.reactionBtn}
         >
-          {/* <img src={} alt="live video" className={styles.ico} /> */}
+          <img src={share} alt="live video" className={styles.ico} />
           Share
         </Button>
       </div>
+
+      <Form className={styles.formComment}>
+        <img src={avatar} className={styles.commentAvatar}/>
+        <Form.Control className={styles.input} placeholder="Write a comment..." />
+        <button type="submit" className={styles.addComment}>
+          <img src={send} alt="post" className={styles.postImg}/>
+        </button>
+      </Form>
     </div>
   );
 };
